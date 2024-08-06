df58455237f3260631148a772aab75b39a1183ad
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-08-06 11:06:56 +0300

    forth commit

--- src/main/java/com/example/SecondFeatureBrunch.java
+++ src/main/java/com/example/SecondFeatureBrunch.java
@@ -4,4 +4,5 @@ public class SecondFeatureBrunch {
     //first commit in second feature brunch
     //second commit in second feature brunch
     //third commit in second feature brunch
+    //forth commit in second feature brunch
 }
