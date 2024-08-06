e4bf90dfadf6e3b25008ccde0fa7d606c76c4de3
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-08-06 10:24:43 +0300

    third commit

--- src/main/java/com/example/SecondFeatureBrunch.java
+++ src/main/java/com/example/SecondFeatureBrunch.java
@@ -3,4 +3,5 @@ package com.example;
 public class SecondFeatureBrunch {
     //first commit in second feature brunch
     //second commit in second feature brunch
+    //third commit in second feature brunch
 }
