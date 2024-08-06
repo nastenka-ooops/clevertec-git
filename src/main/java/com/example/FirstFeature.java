00d5fd866c554f71ecde75f68cd9c0a7a9255657
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-08-06 11:16:44 +0300

    forth commit

--- src/main/java/com/example/FirstFeature.java
+++ src/main/java/com/example/FirstFeature.java
@@ -4,4 +4,5 @@ public class FirstFeature {
     //first commit in first feature brunch
     //second commit in first feature brunch
     //third commit in first feature brunch
+    //forth commit in first feature brunch
 }
